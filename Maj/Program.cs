78803297f7bbfdbfa3e31a4621ce4735ecfddb7c
Nikola Legay <147250203+l-nikola@users.noreply.github.com>
2023-11-29 18:21:11 +0100

    CSharp-TpMAJ

---
+++ Maj/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Maj
+{
+    class Program
+    {
+        public static string maj(string carac)
+        {
+            string nmb;
+            nmb = carac.ToUpper();
+
+            return nmb;
+        }
+        static void Main(string[] args)
+        {
+            string nmb, carac;
+
+            Console.WriteLine("Quels sont les caractères à mettre en majuscule : ");
+            carac = Console.ReadLine();
+
+            nmb = maj(carac);
+
+            Console.WriteLine("Cela donne : " + nmb);
+            Console.ReadLine();
+        }
+    }
+}
